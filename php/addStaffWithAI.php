a122474701f0664d17f19bf22b5190a121d396a6
lordofthebombs <baderwaseem@hotmail.com>
2019-04-03 13:54:02 -0600

    Removed access_pass because to reduce redundancy

--- php/addStaffWithAI.php
+++ php/addStaffWithAI.php
@@ -1,7 +1,5 @@
 <form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
- <!-- Staff ID: <input type = "number" name = "staffID" placeholder = "Enter staff ID.">
-  <br> -->
-  Access pass: <input type = "number" name = "accessPass" placeholder = "Enter acess pass.">
+  Branch ID: <input type = "number" name = "branchID" placeholder = "Enter branch ID.">
   <br>
   First name: <input type = "text" name = "fName" placeholder = "Enter first name.">
   <br>
@@ -18,20 +16,19 @@
   include('dbconnection.php');
 
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      //$staffID = $_POST['staffID'];
-      $accessPass = $_POST['accessPass'];
+      $branchID = $_POST['branchID'];
       $fName = $_POST['fName'];
       $lName = $_POST['lName'];
       $staffType = $_POST['staffType'];
   }
 
-  if (empty($accessPass) || empty($fName) || empty($lName) || empty($staffType)) {
+  if (empty($branchID) || empty($fName) || empty($lName) || empty($staffType)) {
       echo "Please enter all the fields";
   }
   else {
       // Query that adds all this information into the staff table
-      $query = "INSERT INTO staff (staff_id, access_pass, first_name, last_name, staff_type)
-      VALUES (NULL, $accessPass, '$fName', '$lName', '$staffType')";
+      $query = "INSERT INTO staff (NULL, branch_id, first_name, last_name, staff_type)
+      VALUES (NULL, '$fName', '$lName', '$staffType')";
 
       $runQuery = mysqli_query($connection, $query);
 
@@ -42,6 +39,8 @@
           echo "<br>ERROR: Staff not added";
       }
   }
+
+
 
   mysqli_close($connection);
 ?>
