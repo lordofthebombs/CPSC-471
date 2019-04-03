a1ab72e7c5a420de117612e02d03c7116b055273
bader-waseem <25441886+bader-waseem@users.noreply.github.com>
2019-04-02 23:26:53 -0600

    Create addStaff.php

---
+++ php/addStaff.php
@@ -0,0 +1,18 @@
+<form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
+  Staff ID: <input type = "text" name = "staffID" placeholder = "Enter staff ID.">
+  <br>
+  Acess pass: <input type = "text" name = "acessPass" placeholder = "Enter acess pass.">
+  <br>
+  First name: <input type = "text" name = "fName" placeholder = "Enter first name.">
+  <br>
+  Last name: <input type = "text" name = "lName" placeholder = "Enter last name.">
+  <br>
+  Specialty: <input type = "text" name = "specialty" placeholder = "Enter specialty.">
+  <br>
+  Job type: <input type = "text" name = "jobType" placeholder = "Enter job type.">
+  <br>
+  
+  <input type = "submit" class = "btn">
+  
+ </form>
+  
