0c0cdbb98e659991f428d39dfc6e98eca7370ddf
lordofthebombs <baderwaseem@hotmail.com>
2019-04-03 11:42:38 -0600

    Got it to add staff

--- php/addStaff.php
+++ php/addStaff.php
@@ -1,7 +1,7 @@
 <form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
   Staff ID: <input type = "number" name = "staffID" placeholder = "Enter staff ID.">
   <br>
-  Acess pass: <input type = "number" name = "acessPass" placeholder = "Enter acess pass.">
+  Access pass: <input type = "number" name = "accessPass" placeholder = "Enter acess pass.">
   <br>
   First name: <input type = "text" name = "fName" placeholder = "Enter first name.">
   <br>
@@ -10,6 +10,38 @@
   Staff type: <input type = "text" name = "staffType" placeholder = "Enter staff type.">
   <br>
   <input type = "submit" class = "btn">
-  
+
  </form>
-  
+
+<?php
+  // Connecting to database
+  include('dbconnection.php');
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $staffID = $_POST['staffID'];
+      $accessPass = $_POST['accessPass'];
+      $fName = $_POST['fName'];
+      $lName = $_POST['lName'];
+      $staffType = $_POST['staffType'];
+  }
+
+  if (empty($staffID) || empty($accessPass) || empty($fName) || empty($lName) || empty($staffType)) {
+      echo "Please enter all the fields";
+  }
+  else {
+      // Query that adds all this information into the staff table
+      $query = "INSERT INTO staff (staff_id, access_pass, first_name, last_name, staff_type)
+      VALUES ($staffID, $accessPass, '$fName', '$lName', '$staffType')";
+
+      $runQuery = mysqli_query($connection, $query);
+
+      if($runQuery) {
+          echo "<br>Staff added to records";
+      }
+      else {
+          echo "<br>ERROR: Staff not added";
+      }
+  }
+
+  mysqli_close($connection);
+?>
