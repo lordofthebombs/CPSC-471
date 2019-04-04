0e3e342fa08673eaf5d1be140a9d44cbd3e4c57d
lordofthebombs <25441886+lordofthebombs@users.noreply.github.com>
2019-04-03 23:18:07 -0600

    Added functionality to also add the staff to a branch
    Takes the branch ID the staff memebr is going to be assigned and will insert that staff into the works_at table such that they work at the specified branch.

--- php/addStaffWithAI.php
+++ php/addStaffWithAI.php
@@ -1,3 +1,5 @@
+<!-- This works, but you need to have an instance of adoption branch that already has an admin -->
+
 <form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
   Branch ID: <input type = "number" name = "branchID" placeholder = "Enter branch ID.">
   <br>
@@ -23,24 +25,35 @@
   }
 
   if (empty($branchID) || empty($fName) || empty($lName) || empty($staffType)) {
-      echo "Please enter all the fields";
+      echo " Please enter all the fields.";
   }
   else {
       // Query that adds all this information into the staff table
-      $query = "INSERT INTO staff (NULL, branch_id, first_name, last_name, staff_type)
+      $query = "INSERT INTO staff (staff_id, first_name, last_name, staff_type)
       VALUES (NULL, '$fName', '$lName', '$staffType')";
 
       $runQuery = mysqli_query($connection, $query);
 
       if($runQuery) {
-          echo "<br>Staff added to records";
+          echo "<br>Staff added to staff table.";
       }
       else {
-          echo "<br>ERROR: Staff not added";
+          echo "<br>ERROR: Staff not added to staff table.";
+      }
+
+      // Get the most recently added in ID for staff and add it into works_at
+      $lastID = mysqli_insert_id($connection);
+      $query2 = "INSERT INTO works_at (branch_id, staff_id) VALUES ($branchID, $lastID)";
+
+      $runQuery = mysqli_query($connection, $query2);
+
+      if($runQuery) {
+          echo "<br>Staff added to works_at table.";
+      }
+      else {
+          echo "<br>ERROR: Staff not added to works_at table.";
       }
   }
-
-
 
   mysqli_close($connection);
 ?>
