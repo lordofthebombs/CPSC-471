c4cf7a47f667189a7773881ad49b86af40072c57
lordofthebombs <25441886+lordofthebombs@users.noreply.github.com>
2019-04-11 00:16:48 -0600

    Update updateFood.php
    finally works now

--- php/updateFood.php
+++ php/updateFood.php
@@ -23,23 +23,10 @@ $updatedForm = mysqli_query($connection, $query);
         <h2>Edit <Staff></Staff> </h2>
         <form class="form-horizontal" method = "post" action="">
             <?php while($row = mysqli_fetch_array($updatedForm)): ?>
-
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="pwd">Enter First name</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control"  value=<?php echo $row['first_name']?> name="fName">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="pwd">Enter Last name</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" value=<?php echo $row['last_name']?> name="lName">
-                    </div>
-                </div>
                 <div class="form-group">
                     <label class="control-label col-sm-2" for="pwd">Enter Staff Type:</label>
                     <div class="col-sm-10">
-                        <input type="text"  class="form-control" value=<?php echo $row['staff_type']?> name="staffType">
+                        <input type="number"  class="form-control" value=<?php echo $row['quantity']?> name="quantity>
                     </div>
                 </div>
             <?php  endwhile;?>
@@ -47,7 +34,7 @@ $updatedForm = mysqli_query($connection, $query);
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
                     <input type = "submit" class = "btn" name = "submitBtn">
-                    <a href="searchStaff.php"> Go to Staff Table </a>
+                    <a href="foodTable.php"> Go to back to Food Table </a>
                     <a href="adoptionCentreLanding.php"> Go to Landing Page </a>
 
                 </div>
@@ -67,11 +54,11 @@ include('dbconnection.php');
 if(isset($_POST['submitBtn']))
 {
     //raw data not cleaned
-    $first_name = $_POST['fName'];
-    $last_name = $_POST['lName'];
-    $staff_type= $_POST['staffType'];
+    $brand = $_POST['brand'];
+    $animalType = $_POST['animal_type'];
+    $quantity= $_POST['quantity'];
 
-    $query = "UPDATE staff SET first_name = '$first_name', last_name = '$last_name', staff_type = '$staff_type' WHERE staff_id = '$staff_id'";
+    $query = "UPDATE food SET quantity = $quantity WHERE brand = '$brand";
     $run_query = mysqli_query($connection, $query);
 
     if ($run_query) {
